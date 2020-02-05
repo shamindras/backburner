15c7f4f9c699d62093be02598cd272bcf279ee2a
Shamindra Shrotriya <shamindra.shrotriya@gmail.com>
2020-02-05 14:45:58 -0500

    ENH: #3 Update noaa ghcnd extract code

---
+++ R/ghcn-stations-extract.R
@@ -0,0 +1,37 @@
+#' This is a simply wrapper function to extract \code{noaa ghcn}
+#' station, states, and stations metadata as individual tibbles.
+#' This uses the \code{rnoaa} package directly to do this.
+#'
+#' @return This will be list with 3 separate tibbles named
+#' \code{ghcnd_stations, ghcnd_states, ghcnd_countries}
+#'
+#' #' @examples
+#' \dontrun{
+#' ghcnd_stations_metadata <- noaa_ghcn_stations_extract()
+#' }
+#'
+#' @export
+noaa_ghcn_stations_extract <- function(){
+
+    # Download the ghcn stations, states, countries table directly
+    ghcnd_stations  <- rnoaa::ghcnd_stations() %>% tibble::as_tibble(x = .)
+    ghcnd_states    <- rnoaa::ghcnd_states() %>% tibble::as_tibble(x = .)
+    ghcnd_countries <- rnoaa::ghcnd_countries() %>% tibble::as_tibble(x = .)
+
+    # Do renaming of columns for ghcn country table
+    ghcnd_countries <- ghcnd_countries %>%
+                            dplyr::rename(id_fips_code = code,
+                                          country_name = name)
+
+    # Do renaming of columns for ghcn state table
+    ghcnd_states    <- ghcnd_states %>%
+                            dplyr::rename(state_code = code,
+                                      state_name = name)
+
+    # Return output as a list of tibbles
+    out_ghcnd       <- list("ghcnd_stations" = ghcnd_stations,
+                            "ghcnd_states" = ghcnd_states,
+                            "ghcnd_countries" = ghcnd_countries)
+
+    base::return(out_ghcnd)
+}
